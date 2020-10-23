99077507273ad37aaa1066351cba760de90e7fd6
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2020-10-24 01:14:39 +0300

    Add files via upload

---
+++ lab 1(2)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace lr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int R;
+            int x = 2;
+            int y = 1;
+            int z = 4;
+
+            R = 0;
+            Console.WriteLine("Значение переменной y равно 1", y);
+            Console.WriteLine("Значение переменной x равно 2", x);
+            Console.WriteLine("Значение переменной z равно 4", z);
+            Console.WriteLine("выводится решение операции а именно (2 * x + 2 * y - 4 * y - 4 * x * y + z)");
+            int Result = 2 * x + 2 * y - 4 * y - 4 * x * y + z;
+            Console.WriteLine(Result);
+            Console.WriteLine("выведен результат R=-2");
+            Console.WriteLine("для завершения работы программы нажмите любую клавишу...");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("");
+            Console.WriteLine("Выполнил Анцев Никита");
+            Console.WriteLine("Группа Инс-б-о-19-1");
+            Console.WriteLine("21.01.2001");
+            Console.WriteLine("Город Пятигорск");
+            Console.WriteLine("Физика");
+            Console.WriteLine("Любил играть в настольные игры");
+            Console.WriteLine("");
+            Console.WriteLine("для завершения работы программы нажмите любую клавишу...");
+
+
+            Console.ReadKey();
+        }
+    }
+}
